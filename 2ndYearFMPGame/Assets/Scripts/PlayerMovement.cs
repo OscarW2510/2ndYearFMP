25b605e82d7c5fdd42c5fd09bffab6e9f5217cee
OscarW1 <162868@chrome.escg.ac.uk>
2021-03-08 11:56:37 +0000

    Started working on the players health system and added some hearts as a UI but I am yet to make them vanish when getting damaged

--- 2ndYearFMPGame/Assets/Scripts/PlayerMovement.cs
+++ 2ndYearFMPGame/Assets/Scripts/PlayerMovement.cs
@@ -14,6 +14,8 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D myRigidbody;
     private Vector3 change;
     private Animator animator;
+    public FloatValue currentHealth;
+    public Signal playerHealthSignal;
 
     
     void Start()
@@ -78,9 +80,14 @@ public class PlayerMovement : MonoBehaviour
             );
     }
 
-    public void Knock(float knockTime)
+    public void Knock(float knockTime, float damage)
     {
-        StartCoroutine(KnockCo(knockTime));
+        currentHealth.initialValue -= damage;
+        if (currentHealth.initialValue > 0)
+        {
+            playerHealthSignal.Raise();
+            StartCoroutine(KnockCo(knockTime));
+        }
     }
 
     private IEnumerator KnockCo(float knockTime)
