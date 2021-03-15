06b0c08ce09d5b93beac8b3b4a2db5da73f21078
OscarW1 <162868@chrome.escg.ac.uk>
2021-03-15 18:30:00 +0000

    Made a ew room and transition however it is a little broken but shouldn't be to hard to fix

---
+++ 2ndYearFMPGame/Assets/Scripts/ScriptableObjects/VectorValue2.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class VectorValue2 : ScriptableObject
+{
+    public Vector2 initialValue;
+
+}
