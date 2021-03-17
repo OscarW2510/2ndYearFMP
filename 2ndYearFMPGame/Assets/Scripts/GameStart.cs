e241463df5b3a217c46e483404514cbfc7898b17
OscarW1 <162868@chrome.escg.ac.uk>
2021-03-17 15:58:50 +0000

    Done a lot of art changes and i added a fade in and out to the houses

---
+++ 2ndYearFMPGame/Assets/Scripts/GameStart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour
+{
+    static GameStart instance;
+    public Vector2 initialPlayerPosition;
+    public Vector2 initialCameraMin;
+    public Vector2 initialCameraMax;
+    public VectorValue playerStorage;
+    public Vector2Value cameraStorage;
+
+    public void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(this);
+
+        playerStorage.initialValue = initialPlayerPosition;
+        cameraStorage.minValue = initialCameraMin;
+        cameraStorage.maxValue = initialCameraMax;
+    }
+}
--- 2ndYearFMPGame/Assets/Scripts/SceneTransition.cs
+++ 2ndYearFMPGame/Assets/Scripts/SceneTransition.cs
@@ -11,13 +11,40 @@ public class SceneTransition : MonoBehaviour
     public Vector2Value cameraStorage;
     public Vector2 cameraMinValue;
     public Vector2 cameraMaxValue;
+    public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
+    public float fadeWait;
+
+    private void Awake()
+    {
+        if(fadeInPanel != null)
+        {
+            GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1);
+        }
+    }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && !other.isTrigger)
         {
             playerStorage.initialValue = playerPosition;
-            SceneManager.LoadScene(sceneToLoad);
+            StartCoroutine(FadeCo());
+            //SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+
+    public IEnumerator FadeCo()
+    {
+        if(fadeOutPanel != null)
+        {
+            Instantiate(fadeOutPanel, Vector3.zero, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(fadeWait);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneToLoad);
+        while (!asyncOperation.isDone)
+        {
+            yield return null;
         }
     }
 }
