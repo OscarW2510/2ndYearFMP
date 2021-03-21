76125d58f9dc4f33572b8cc42b47f9f8c6bd12ca
OscarW1 <162868@chrome.escg.ac.uk>
2021-03-21 22:06:21 +0000

    I added patroling enemies and had to redo the entire of the 'The Forest Ground Bottom' as I deleted it by accident

--- 2ndYearFMPGame/Assets/Scripts/EnemyScripts/Log.cs
+++ 2ndYearFMPGame/Assets/Scripts/EnemyScripts/Log.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Log : Enemy
 {
-    private Rigidbody2D myRigidbody;
+    public Rigidbody2D myRigidbody;
     public Transform target;
     public float chaseRadius;
     public float attackRadius;
@@ -19,6 +19,7 @@ public class Log : Enemy
         myRigidbody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         target = GameObject.FindWithTag("Player").transform;
+        anim.SetBool("wakeUp", true);
     }
 
     // Update is called once per frame
@@ -27,7 +28,7 @@ public class Log : Enemy
         CheckDistance();  
     }
 
-    void CheckDistance()
+    public virtual void CheckDistance()
     {
         if(Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRadius)
         {
@@ -52,7 +53,7 @@ public class Log : Enemy
         anim.SetFloat("moveY", setVector.y);
     }
 
-    private void changeAnim(Vector2 direction)
+    public void changeAnim(Vector2 direction)
     {
         if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
         {
