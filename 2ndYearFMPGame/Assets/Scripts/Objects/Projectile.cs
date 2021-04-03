56148bf4fa12c0b3b507936e8b555b0b04b42097
OscarW1 <162868@chrome.escg.ac.uk>
2021-04-03 16:59:25 +0100

    made a turret enemy and editied the cave a little

---
+++ 2ndYearFMPGame/Assets/Scripts/Objects/Projectile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float moveSpeed;
+    public Vector2 directionToMove;
+    public float lifetime;
+    private float lifetimeSeconds;
+    public Rigidbody2D myRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        lifetimeSeconds = lifetime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifetimeSeconds -= Time.deltaTime;
+        if (lifetimeSeconds <=0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void Launch(Vector2 initialVel)
+    {
+        myRigidBody.velocity = initialVel * moveSpeed;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(this.gameObject);
+    }
+}
