5fa0768cce851bbe3632af1148b30585610006a8
OscarW1 <162868@chrome.escg.ac.uk>
2021-04-07 18:06:23 +0100

    made a boss health bar work

---
+++ 2ndYearFMPGame/Assets/HealthBar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Enemy boss;
+
+    public Slider healthBar;
+
+    private bool active;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+           active = true;
+           healthBar.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        active = false;
+        healthBar.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (active)
+        {
+            healthBar.value = boss.health / boss.maxHealth.initialValue;
+        }
+    }
+}
