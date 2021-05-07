fb1b7ebc7f0937a8fa57acae6e1f3d92811ed01a
OscarW1 <162868@chrome.escg.ac.uk>
2021-05-07 22:48:33 +0100

    added a controls thing to pop up when you start the game and editied the enemy settings

---
+++ 2ndYearFMPGame/Assets/Scripts/Menu Stuff/ControlsMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsMenu : MonoBehaviour
+{
+    public GameObject controls;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controls.SetActive(false);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            controls.SetActive(true);
+            StartCoroutine("WaitForSec");
+            //Time.timeScale = (Time.timeScale == 1) ? 0 : 1;
+        }
+    }
+
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(7);
+        Destroy(controls);
+        Destroy(gameObject);
+        //Time.timeScale = 1;
+    }
+}
