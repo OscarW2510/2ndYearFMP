51322185e543d0a6b1fd13b8b8744f2895078625
OscarW1 <162868@chrome.escg.ac.uk>
2021-05-08 22:11:06 +0100

    made the level complete scene and made a trigger for when you kill the boss you go to the scene

---
+++ 2ndYearFMPGame/Assets/Scripts/Menu Stuff/LevelComplete.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelComplete : MonoBehaviour
+{
+    public GameObject levelComplete;
+    //public VectorValue playerPosition;
+    public void GotToMainMenu()
+    {
+        //playerPosition.initialValue = FindObjectOfType<PlayerMovement>().transform.position;
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
